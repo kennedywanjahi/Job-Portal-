f089fcc4fbe6797f8916f7d34547ab3378b8f8c4
ken <kennedywanjahi@gmail.com>
2018-09-14 12:07:35 +0300

    module for job seeking users

---
+++ jobSeekers.php
@@ -0,0 +1,157 @@
+<?php include 'includes/header.php'; ?>
+<div class="container">
+    <div class="row job-posting wow fadeInUp" data-wow-delay="1s">
+        <div role="tabpanel">
+          <!-- Nav tabs -->
+          <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation"><a href="#employeers" aria-controls="profile" role="tab" data-toggle="tab">Employeers</a></li>
+          </ul>
+
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <div role="tabpanel" class="tab-pane fade in active" id="employeers">
+                <ul class="list-inline">
+                    <li>
+                        <a href="">
+                            <img src="img/employee4.png" alt="">
+                            <div class="overlay"><h3>Instagram</h3></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/employee5.png" alt="">
+                            <div class="overlay"><h3>Microsoft</h3></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/employee6.png" alt="">
+                            <div class="overlay"><h3>Dribbble</h3></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/employee1.png" alt="">
+                            <div class="overlay"><h3>Beats Music</h3></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/employee2.png" alt="">
+                            <div class="overlay"><h3>Facebook</h3></div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="img/employee3.png" alt="">
+                            <div class="overlay"><h3>Twitter</h3></div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+          </div>
+
+        </div>
+    </div>
+</div>
+<hr>
+
+<div class="container">
+    <div class="row page-title text-center wow bounce"  data-wow-delay="1s">
+        <h5>Recent Jobs</h5>
+        <h2><span>54716</span> Available jobs for you</h2>
+    </div>
+    <div class="row jobs">
+        <div class="col-md-9">
+            <div class="job-posts table-responsive">
+                <table class="table">
+                    <tr class="odd wow fadeInUp" data-wow-delay="1s">
+                        <td class="tbl-logo"><img src="img/job-logo1.png" alt=""></td>
+                        <td class="tbl-title"><h4>Web Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>dribbble community</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="even wow fadeInUp" data-wow-delay="1.1s">
+                        <td class="tbl-logo"><img src="img/job-logo2.png" alt=""></td>
+                        <td class="tbl-title"><h4>Front End Developer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Jolil corporation</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="odd wow fadeInUp" data-wow-delay="1.2s">
+                        <td class="tbl-logo"><img src="img/job-logo3.png" alt=""></td>
+                        <td class="tbl-title"><h4>HR Manager <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Fanta bevarage</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="even wow fadeInUp" data-wow-delay="1.3s">
+                        <td class="tbl-logo"><img src="img/job-logo4.png" alt=""></td>
+                        <td class="tbl-title"><h4>Internship Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Google</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="odd wow fadeInUp" data-wow-delay="1.4s">
+                        <td class="tbl-logo"><img src="img/job-logo5.png" alt=""></td>
+                        <td class="tbl-title"><h4>Software Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Microsoft</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="even hide-jobs">
+                        <td class="tbl-logo"><img src="img/job-logo4.png" alt=""></td>
+                        <td class="tbl-title"><h4>Internship Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Google</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="odd hide-jobs">
+                        <td class="tbl-logo"><img src="img/job-logo5.png" alt=""></td>
+                        <td class="tbl-title"><h4>Software Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Microsoft</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="even hide-jobs">
+                        <td class="tbl-logo"><img src="img/job-logo4.png" alt=""></td>
+                        <td class="tbl-title"><h4>Internship Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Google</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                    <tr class="odd hide-jobs">
+                        <td class="tbl-logo"><img src="img/job-logo5.png" alt=""></td>
+                        <td class="tbl-title"><h4>Software Designer <br><span class="job-type">full time</span></h4></td>
+                        <td><p>Microsoft</p></td>
+                        <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
+                        <td><p>&dollar; 14000</p></td>
+                        <td class="tbl-apply"><a href="#">Apply now</a></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="more-jobs">
+                <a href=""> <i class="fa fa-refresh"></i>View more jobs</a>
+            </div>
+        </div>
+        <div class="col-md-3 hidden-sm">
+            <div class="job-add wow fadeInRight" data-wow-delay="1.5s">
+                <h2>Seeking a job?</h2>
+                <a href="#">Create a Account</a>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+<?php
+include 'includes/footer.php';
+ ?>
