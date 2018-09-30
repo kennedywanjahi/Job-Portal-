f400a788f7e6da8a649b81e57cb8527f8c85b9a5
ken <kennedywanjahi@gmail.com>
2018-09-30 22:48:14 +0300

    job seeker job views and apllication module

---
+++ includes/view_all_jobs.php
@@ -0,0 +1,31 @@
+
+<div class="container">
+    <div class="row page-title text-center wow bounce"  data-wow-delay="1s">
+        <h5>Recent Jobs</h5>
+        <h2><span>54716</span> Available jobs for you</h2>
+    </div>
+    <div class="row jobs">
+        <div class="col-md-9">
+            <div class="job-posts table-responsive">
+                <table class="table">
+                    <tr class="odd wow fadeInUp" data-wow-delay="1s">
+                        <?php
+                          ViewJobs();
+                          ?>
+                    </tr>
+                </table>
+            </div>
+            <div class="more-jobs">
+                <a href=""> <i class="fa fa-refresh"></i>View more jobs</a>
+            </div>
+        </div>
+        <div class="col-md-3 hidden-sm">
+            <div class="job-add wow fadeInRight" data-wow-delay="1.5s">
+                <h2>Seeking a job?</h2>
+                <a href="account.php?source=register">Create a Account</a>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+</div>
