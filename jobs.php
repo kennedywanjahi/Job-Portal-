1c5b99d637f2c6f2eee5be9e404f4bf91cb8bcba
ken <kennedywanjahi@gmail.com>
2018-10-01 10:11:14 +0300

    more jobs funtionalities

--- jobs.php
+++ jobs.php
@@ -1,112 +1,48 @@
 <?php
 
-    include 'header.php';
+    require 'header.php';
+    include 'functions.php';
+    include 'employers/includes/db.php';
 
 
 
 
 ?>
 
-            <div class="container">
-                <div class="row page-title text-center wow bounce"  data-wow-delay="1s">
-                    <h5>Recent Jobs</h5>
-                    <h2><span>54716</span> Available jobs for you</h2>
-                </div>
-                <div class="row jobs">
-                    <div class="col-md-9">
-                        <div class="job-posts table-responsive">
-                            <table class="table">
-                                <tr class="odd wow fadeInUp" data-wow-delay="1s">
-                                    <td class="tbl-logo"><img src="img/job-logo1.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Web Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>dribbble community</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="even wow fadeInUp" data-wow-delay="1.1s">
-                                    <td class="tbl-logo"><img src="img/job-logo2.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Front End Developer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Jolil corporation</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="odd wow fadeInUp" data-wow-delay="1.2s">
-                                    <td class="tbl-logo"><img src="img/job-logo3.png" alt=""></td>
-                                    <td class="tbl-title"><h4>HR Manager <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Fanta bevarage</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="even wow fadeInUp" data-wow-delay="1.3s">
-                                    <td class="tbl-logo"><img src="img/job-logo4.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Internship Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Google</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="odd wow fadeInUp" data-wow-delay="1.4s">
-                                    <td class="tbl-logo"><img src="img/job-logo5.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Software Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Microsoft</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="even hide-jobs">
-                                    <td class="tbl-logo"><img src="img/job-logo4.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Internship Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Google</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="odd hide-jobs">
-                                    <td class="tbl-logo"><img src="img/job-logo5.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Software Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Microsoft</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="even hide-jobs">
-                                    <td class="tbl-logo"><img src="img/job-logo4.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Internship Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Google</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                                <tr class="odd hide-jobs">
-                                    <td class="tbl-logo"><img src="img/job-logo5.png" alt=""></td>
-                                    <td class="tbl-title"><h4>Software Designer <br><span class="job-type">full time</span></h4></td>
-                                    <td><p>Microsoft</p></td>
-                                    <td><p><i class="icon-location"></i>San Franciso, USA</p></td>
-                                    <td><p>&dollar; 14000</p></td>
-                                    <td class="tbl-apply"><a href="#">Apply now</a></td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div class="more-jobs">
-                            <a href=""> <i class="fa fa-refresh"></i>View more jobs</a>
-                        </div>
-                    </div>
-                    <div class="col-md-3 hidden-sm">
-                        <div class="job-add wow fadeInRight" data-wow-delay="1.5s">
-                            <h2>Seeking a job?</h2>
-                            <a href="#">Create a Account</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
+<?php
+if (isset($_GET['source'])) {
+  $source = escape($_GET['source']);
+}else {
+  $source = '';
+}
+  switch ($source) {
+      default:
+      include 'includes/view_all_jobs.php';
+      break;
+  }
+
+ ?>
+<?php if (isset($_GET['job_id'])){
+                echo $job_id = escape($_GET['job_id']);
+                echo $companyName = escape($_GET['company']);
+                echo $jobPosition = escape($_GET['position']);
+                if (isset($_SESSION['current_user'])) {
+                  $mobile = $_SESSION['user_mobile'];
+                  $name = $_SESSION['current_user'];
+	                 $query = "INSERT INTO applications SET JobID='$job_id',
+                             Company_Name='$companyName', Position='$jobPosition',
+                             Name='$name', UserMobile='$mobile'";
+                  $applyJob = mysqli_query($connection, $query);
+                  confirmQuery($applyJob);
+                  echo "<script>alert('Position Appllied successfully');</script>";
+                  echo '<script>window.location="jobs.php" </script>';
+
+                    }
+      }
+?>
 
 
 
-        </div>
         <?php
 
         include 'includes/footer.php'; ?>
