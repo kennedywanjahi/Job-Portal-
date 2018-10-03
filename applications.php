6de43a4b9b87104d8a331109fc0786bb1a74bafc
ken <kennedywanjahi@gmail.com>
2018-10-03 12:35:34 +0300

    job seeker job applications withdrawal

--- applications.php
+++ applications.php
@@ -10,10 +10,6 @@
 
 
 <div class="container">
-    <div class="row page-title text-center wow bounce"  data-wow-delay="1s">
-        <h5>Recent Jobs</h5>
-        <h2><span>54716</span> Available jobs for you</h2>
-    </div>
     <div class="row jobs">
         <div class="col-md-9">
             <div class="job-posts table-responsive">
@@ -25,14 +21,8 @@
                     </tr>
                 </table>
             </div>
-            <div class="more-jobs">
-                <a href=""> <i class="fa fa-refresh"></i>View more jobs</a>
-            </div>
         </div>
         <div class="col-md-3 hidden-sm">
-            <div class="job-add wow fadeInRight" data-wow-delay="1.5s">
-                <h2>Seeking a job?</h2>
-            </div>
         </div>
     </div>
 </div>
